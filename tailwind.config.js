522e442d43138a41a8cc244d6928d38d181f084c
Matias <matias.szbar@gmail.com>
2021-06-20 16:03:57 +0200

    Change blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'light-blue': colors.lightBlue,
+                'light-blue': colors.sky,
                 cyan: colors.cyan,
             },
         },
